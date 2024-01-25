a2ac33b2b7bf265d3e6c9a14167e1ec5695966e8
Alberto Caro <albertocarobarrera@gmail.com>
2024-01-25 12:46:44 +0100

    añadiendo busqueda avanzada

--- hotel/api_urls.py
+++ hotel/api_urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('clientes',cliente_list),
     path('reservas',reserva_list),
     path('cliente_buscar',cliente_buscar),
+    path('cliente_busqueda_avanzada',cliente_busqueda_avanzada),
 ]
--- hotel/api_views.py
+++ hotel/api_views.py
@@ -2,9 +2,9 @@ from .models import *
 from .serializers import *
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .forms import *
-from django.db.models import Q
 from rest_framework import status
+from .forms import *
+from django.db.models import Q,Prefetch
 
 @api_view(['GET'])
 def cliente_list(request):
@@ -30,4 +30,35 @@ def cliente_buscar(request):
         return Response(serializers.data)
     else:
         return Response(formulario.errors,status=status.HTTP_400_BAD_REQUEST)
-    
+
+@api_view(['GET'])
+def cliente_busqueda_avanzada(request):
+
+    if (len(request.query_params)>0):
+        formulario = BusquedaAvanzadaClienteForm(request.query_params)
+        if formulario.is_valid():
+            mensaje="Se ha buscado por:\n"
+            
+            QScliente = Cliente.objects
+            
+            textoBusqueda=formulario.cleaned_data.get('textoBusqueda')
+            telefono=formulario.cleaned_data.get('telefono')
+
+            if textoBusqueda is not None:
+                QScliente = QScliente.filter(Q(nombre__contains=textoBusqueda) | Q(correo_electronico__contains=textoBusqueda) | Q(direccion__contains=textoBusqueda))
+                mensaje+=" Contiene: "+ textoBusqueda+"\n"
+            
+            if telefono is not None:
+                QScliente = QScliente.filter(telefono__startswith=telefono)
+                mensaje+= str(telefono)+"\n"
+            
+            cliente = QScliente.all()
+            
+            serializer = ClienteSerializer(cliente,many=True)
+
+            return Response(serializer.data)
+            
+        else:
+            return Response(formulario.errors, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response({}, status=status.HTTP_400_BAD_REQUEST)
